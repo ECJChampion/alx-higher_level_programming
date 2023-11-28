c2320714a1edd2d24c28ff2fafe96c6984722364
Elekwachi Champion <championchukwudi@gmail.com>
2023-11-28 07:58:53 -0800

    function that creates a copy of the string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+!#/usr/bin/python3
+def remove_char_at(s, n):
+    if n < 0 or n >= len(s):
+        return "Index out of range"
+
+    return s[:n] + s[n+1:]
+
+# Example usage:
+original_string = "example"
+index_to_remove = 3
+result = remove_char_at(original_string, index_to_remove)
+print(result)  # Output: "exaple"
+
