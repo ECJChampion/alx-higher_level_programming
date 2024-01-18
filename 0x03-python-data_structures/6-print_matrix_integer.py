bcd7aaeb31e922c1bcbc2a50c21bb0dbb4b72cce
Elekwachi Champion <championchukwudi@gmail.com>
2024-01-18 02:45:02 -0800

    a function that prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# function that prints a matrix of integers.
+
+def print_matrix_integer(matrix=[[]]):
+    if type(matrix) == list:
+        for row in matrix:
+            separator = ""
+            for item in row:
+                if separator:
+                    print(separator, end="")
+                print("{:d}".format(item), end="")
+                separator = " "
+            print()
